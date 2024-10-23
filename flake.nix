655b79a2e4912cae92744bedf11f69a1587b6841
Kelvin Steiner <me@steinerkelvin.dev>
2024-10-23 16:18:47 -0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3 # Python 3.11
+            poetry
+          ];
+        };
+      }
+    );
+}
